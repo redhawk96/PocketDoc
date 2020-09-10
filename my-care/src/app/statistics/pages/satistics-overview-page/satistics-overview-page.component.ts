e2a6a3e31dbbf5414d3dfb56e50c3198b2ba6767
redhawk96 <uditha1003@gmail.com>
2020-09-10 17:13:00 +0530

    Statistics page implemented

--- my-care/src/app/statistics/pages/satistics-overview-page/satistics-overview-page.component.ts
+++ my-care/src/app/statistics/pages/satistics-overview-page/satistics-overview-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { Application } from '@nativescript/core';
+import { DeptEpidemicService } from '~/lib/base/services/gov.service';
+import { DepartmentEpidemicProfile } from '~/lib/base/models/DepartmentEpidemicProfile';
+import { extractEpidemicProfile } from '~/lib/base/utils/deptEpidemicUtils';
+// tslint:disable no-empty
 
 @Component({
-  selector: 'app-satistics-overview-page',
-  templateUrl: './satistics-overview-page.component.html',
-  styleUrls: ['./satistics-overview-page.component.css']
+    selector: 'app-satistics-overview-page',
+    templateUrl: './satistics-overview-page.component.html',
+    styleUrls: ['./satistics-overview-page.component.css']
 })
 export class SatisticsOverviewPageComponent implements OnInit {
 
-  constructor() { }
+    epidemicProfile : DepartmentEpidemicProfile;
 
-  ngOnInit(): void {
-  }
+    constructor(private deptEpidemicService: DeptEpidemicService) {
+        this.deptEpidemicService.fetchEpidemicProfile().subscribe((eProfile : any) => {
+            this.epidemicProfile = extractEpidemicProfile(eProfile.data);
+        })
+    }
 
+
+    ngOnInit(): void {
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>Application.getRootView();
+        sideDrawer.showDrawer();
+    }
 }
