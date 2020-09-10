829098975b1e41ce98189033d399ea15fb9bd788
redhawk96 <uditha1003@gmail.com>
2020-09-10 12:02:17 +0530

    Precautions and Symptoms page implemented

--- my-care/src/app/epidemic/pages/precautions-page/precautions-page.component.ts
+++ my-care/src/app/epidemic/pages/precautions-page/precautions-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { PocketDocEpidemicService } from '~/lib/base/services/pocketdoc.service';
+import { RouterExtensions } from '@nativescript/angular';
+// tslint:disable: no-empty
 
 @Component({
-  selector: 'app-precautions-page',
-  templateUrl: './precautions-page.component.html',
-  styleUrls: ['./precautions-page.component.css']
+    selector: 'app-precautions-page',
+    templateUrl: './precautions-page.component.html',
+    styleUrls: ['./precautions-page.component.css']
 })
 export class PrecautionsPageComponent implements OnInit {
 
-  constructor() { }
+    precautions = [];
 
-  ngOnInit(): void {
-  }
+    constructor(private pocketDocEpidemicService: PocketDocEpidemicService, private router: RouterExtensions) {
+        this.pocketDocEpidemicService.getEpidemicProfile().subscribe((eProfile: any) => {
+            this.precautions = eProfile.precautions;
+        })
+    }
+
+    ngOnInit(): void {
+    }
+
+    goBack() {
+        this.router.navigate(['/epidemic'])
+    }
 
 }
